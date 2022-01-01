6b2d9088d5b09f218fe090249f93746f78269ab5
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-01-01 21:47:51 +0530

    Update q01.py

--- Chapter_2/q01.py
+++ Chapter_2/q01.py
@@ -1,1 +1,17 @@
-## Personal Information
+# 1.Personal Information
+
+Name = input("Enter your name: ")
+Address = input("Enter your address, with city, state and ZIP: ")
+Phone = input("Enter your phone number: ")
+col_major = input("Enter your college major: ")
+
+
+
+# Show the information that is provided by user.
+#\n - Causes output to be advanced to the next line.
+print("\nYour information is below: ")
+
+print("\nYour name:", Name)
+print("Your address is:", Address)
+print("Your phone number is:", Phone)
+print("Your major is:", col_major)
