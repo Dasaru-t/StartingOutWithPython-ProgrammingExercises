539351a74dda189cb9b8768d4831646ea0c5e384
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-01-02 21:03:09 +0530

    Created q02.py

---
+++ Chapter_2/q02.py
@@ -0,0 +1,10 @@
+# 2. Sales Prediction
+
+total_sales = float(input('Enter projected amount of total sales: $ '))
+
+# 23% equal to 0.23 (23/100 = 0.23)
+profit = total_sales * 0.23
+
+#string '.2f', is the format specifier, .2 specifies the precision and f specifies that the data type of the number we are formatting (float).
+#to be formatted with comma separators, you can insert a comma into the format specifier.
+print('Annual profit is: $',format(profit, ',.2f'))
