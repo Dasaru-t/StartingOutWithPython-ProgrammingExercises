5da649e96f784ebcbeb848f53b92d4041119a7bf
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-01-03 12:28:54 +0530

    Created q05.py

---
+++ Chapter_2/q05.py
@@ -0,0 +1,12 @@
+# 5. Distance Traveled
+
+# Distance  =  Speed x Time
+speed = 70
+time1 = 6
+time2 = 10
+time3 = 15
+
+print('The distance car will travel in', time1 ,'hours: ',speed * time1, 'Miles')
+print('The distance car will travel in', time2 ,'hours: ',speed * time2, 'Miles')
+print('The distance car will travel in', time3 ,'hours: ',speed * time3, 'Miles')
+
