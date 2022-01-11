1ebe15cd32aa8701e9ae5f60265249ec96e23db7
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-01-11 17:25:11 +0530

    Created q01.py

---
+++ Chapter_4/q01.py
@@ -0,0 +1,11 @@
+#1. Bug Collector
+
+# Initialize an accumulator variable.
+total = 0
+
+# Get the numbers and accumulate them.
+for day in ['Monday','Tuesday','Wednsday','Thursday','Friday']:
+  bugs = int(input("Enter the number of bugs collected for " +day+" : "))
+  total += bugs   #Augmented assignment operators. equal to (total = total + bugs)
+
+print('\nTotal number of bugs collected: ',total)
