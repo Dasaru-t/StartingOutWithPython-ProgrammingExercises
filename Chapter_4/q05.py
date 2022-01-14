f83aeb1513f7202a5656f43671bdaa76b833f74b
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-01-14 09:55:10 +0530

    Created q05.py

---
+++ Chapter_4/q05.py
@@ -0,0 +1,23 @@
+# 5. Average Rainfall
+
+#ask for the number of years
+years = int(input("Enter number of years: "))
+
+months = 12
+total = 0   # Initialize an accumulator variable.
+
+for y in range(years):
+  for m in range(months):
+    #print("Inches of rainfall for year",(y + 1),"month",(m + 1))
+    rainfall = float(input("Inches of rainfall for year "+str(y + 1)+" month "+str(m + 1)+" : ")) #ask for the inches of rainfall for that month.
+    total += rainfall #calculating running total
+  print()
+
+#Calculating duration in months & average rainfall
+duration_in_months = months * years 
+average_rainfall = total / duration_in_months
+
+# Display the results
+print("\nNumber of months:",duration_in_months)
+print("Total inches of rainfall:",format(total,".2f"))
+print("Average rainfall per month",format(average_rainfall,".2f"))
