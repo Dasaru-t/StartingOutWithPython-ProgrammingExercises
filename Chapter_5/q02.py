57b568ca61afde6fe738f104116f0868e88324df
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-01-20 13:18:04 +0530

    Updated q02.py

--- Chapter_5/q02.py
+++ Chapter_5/q02.py
@@ -1,29 +1,52 @@
 # 2. Sales Tax Program Refactoring
 
+#Global costant
+STATE_TAX_PERCENTAGE = 0.05
+COUNTRY_TAX_PERCENTAGE = 0.025
 
-# Create global variables.
-purchase = 0
-statetax = purchase * 0.05 
-countrytax = purchase * 0.025
-totaltax = countrytax + statetax
-
+# The main function.
 def main():
-  # Get the purchase amount
-  purchase = float(input('Enter amount of a purchase: '))
-  print('\nAmount of the purchase: $',format(purchase,',.2f'))
-  state_tax()
-  country_tax()
-  total_tax()
-  print('Total sales: $',format(purchase + totaltax,',.2f'))
+  #Get the amount of a purchase
+  amount = get_amount()
+  # Calculate the state tax
+  state = state_tax(amount)
+  # Calculate the country tax
+  country = country_tax(amount)
+  # Calculate the total tax
+  total = total_tax(state,country)
 
-def state_tax():
-  print('State tax: $',format(statetax,',.2f'))
+  #Display the results
+  print('\nAmount of the purchase: $',format(amount,',.2f'))
+  print('State tax: $',format(state,',.2f'))
+  print('Country tax: $',format(country,',.2f'))
+  print('Total sales tax: $',format(total,',.2f'))
+  print('Total sales: $',format(amount + total,',.2f')) #Calculate total sales
 
-def country_tax():
-  print('Country tax: $',format(countrytax,',.2f'))
+# The get_amount function prompts the
+# user to enter an amount of a purchase and it
+# returns that value.
+def get_amount():
+  price = float(input("Enter amount of a purchase: "))
+  return price
 
-def total_tax():
-  print('Total sales tax: $',format(totaltax,',.2f'))
+# The state_tax function accepts an amount
+# as an argument and returns the state tax
+# specified by STATE_TAX_PERCENTAGE.
+def state_tax(amount):
+  return amount * STATE_TAX_PERCENTAGE
+
+
+# The country_tax function accepts an amount
+# as an argument and returns the country tax
+# specified by COUNTRY_TAX_PERCENTAGE.
+def country_tax(amount):
+  return amount * COUNTRY_TAX_PERCENTAGE
+
+# The total_tax function accepts an state and country
+# as arguments and returns the total tax
+def total_tax(state,country):
+  return (state + country)
+
 
 # Call the main function.
 main()
