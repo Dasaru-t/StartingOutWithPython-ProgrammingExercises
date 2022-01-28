e1510f740542e2023f76892101545706f8592a36
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-01-28 14:18:18 +0530

    Created add_coffee_record.py

---
+++ Chapter_6/add_coffee_record.py
@@ -0,0 +1,34 @@
+# In the Spotlight: Adding and Displaying Records
+
+# This program adds coffee inventory records to
+# the coffee.txt file.
+
+def main():
+  # Create a variable to control the loop.
+  another = 'y'
+
+  # Open the coffee.txt file in append mode.
+  coffee_file = open('coffee.txt','a')
+
+  # Add records to the file.
+  while another == 'y' or another == 'Y':
+    # Get the coffee record data.
+    print('Enter the following coffee data:')
+    descr = input('Discription: ')
+    qty = int(input('Quantity (in pounds): '))
+
+    # Append the data to the file
+    coffee_file.write(descr + '\n')
+    coffee_file.write(str(qty) + '\n')
+
+    # Determine whether the user wants to add
+    # another record to the file.
+    print('Do you want to add another record?')
+    another = input('y = yes, anything else = no: ')
+
+  # close the file.
+  coffee_file.close()
+  print('Data appended to coffee.txt.')
+
+# call the main function.
+main()
