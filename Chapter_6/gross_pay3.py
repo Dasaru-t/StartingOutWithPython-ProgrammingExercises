68d9fdc3dead2b887f67ac3cbfac16163feea59a
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-02-02 10:39:36 +0530

    Created gross_pay3.py

---
+++ Chapter_6/gross_pay3.py
@@ -0,0 +1,23 @@
+# Displaying an Exception’s Default Error Message
+# gross_pay3.py
+# This program calculates gross pay.
+
+def main():
+  try:
+    # Get the number of hours worked.
+    hours = int(input('How many hours did you work? '))
+
+    # Get the hourly pay rate.
+    pay_rate = float(input('Enter your hourly pay rate: '))
+
+    # Calculate the gross pay.
+    gross_pay = hours * pay_rate
+
+    # Display the gross pay.
+    print('Gross pay: $', format(gross_pay, ',.2f'), sep='')
+
+  except ValueError as err:
+    print(err)
+    
+# Call the main function.
+main()
