f00122e655a8c1b9432b0ae9a7c51bcbc2b81e4f
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-02-04 14:00:16 +0530

    Created sort_list.py

---
+++ Chapter_7/sort_list.py
@@ -0,0 +1,15 @@
+# The sort method rearranges the elements of a list so they appear in ascending order 
+# (from the lowest value to the highest value).
+
+my_list1 = [9, 1, 0, 2, 8, 6, 7, 4, 5, 3]
+print('Original order:', my_list1)
+
+my_list1.sort()
+print('Sorted order:',my_list1)
+
+
+my_list = ['beta', 'alpha', 'delta', 'gamma']
+print('Original order:', my_list)
+
+my_list.sort()
+print('Sorted order:', my_list)
