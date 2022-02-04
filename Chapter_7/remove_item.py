de0dae04d04170654134d6e0f6329ed3db6b12ed
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-02-04 14:07:34 +0530

    Created remove_item.py

---
+++ Chapter_7/remove_item.py
@@ -0,0 +1,28 @@
+# remove_item.py
+# This program demonstrates how to use the remove
+# method to remove an item from a list.
+
+def main():
+  # Create a list with some items.
+  food = ['Pizza', 'Burgers', 'Chips']
+
+  # Display the list.
+  print('Here are the items in the food list:')
+  print(food)
+
+  # Get the item to change.
+  item = input('which item should I remove? ')
+
+  try:
+    # Remove the item.
+    food.remove(item)
+
+    # Display the list.
+    print('Here is the revised list:')
+    print(food)
+
+  except ValueError:
+    print('That item was not found in the list.')
+
+# Call the main function.
+main()
