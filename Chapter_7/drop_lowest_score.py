f8f3640ba686a3bf03a4da243a06903dff110ae1
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-02-10 00:37:57 +0530

    Updated drop_lowest_score.py

--- Chapter_7/drop_lowest_score.py
+++ Chapter_7/drop_lowest_score.py
@@ -25,3 +25,30 @@ def main():
 
   # Display the average.
   print('The average, with the lowest score dropped is:', average)
+
+
+# The get_scores function gets a series of test
+# scores from the user and stores them in a list.
+# A reference to the list is returned.
+
+def get_scores():
+  # Create an empty list.
+  test_scores = []
+
+  # Create a variable to control the loop.
+  again = 'y'
+
+  # Get the scores from the user and add them to
+  # the list.
+  while again == 'y':
+    # Get a score and add it to the list.
+    value = float(input("Enter a test score: "))
+    test_scores.append(value)
+
+    # Want to do this again?
+    print('Do you want to add another score?')
+    again = input("y = yes, anything else = no: ")
+    print()
+
+  # Return the list.
+  return test_scores
