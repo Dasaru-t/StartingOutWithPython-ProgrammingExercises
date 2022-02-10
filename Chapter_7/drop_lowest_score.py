2ea308b4415520d2e9ef04dedd6a0037086ab3d7
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-02-10 22:15:34 +0530

    Updated drop_lowest_score.py

--- Chapter_7/drop_lowest_score.py
+++ Chapter_7/drop_lowest_score.py
@@ -52,3 +52,21 @@ def get_scores():
 
   # Return the list.
   return test_scores
+
+
+# The get_total function accepts a list as an
+# argument returns the total of the values in
+# the list.
+def get_total(value_list):
+  # Create a variable to use as an accumulator.
+  total = 0.0
+
+  # Calculate the total of the list elements.
+  for num in value_list:
+    total += num
+
+  # Return the total.
+  return total
+
+# Call the main function.
+main()
