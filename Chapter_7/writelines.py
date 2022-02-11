96f1649f180e181eac886e3ff581da8786d7c280
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-02-11 23:38:50 +0530

    Created writelines.py

---
+++ Chapter_7/writelines.py
@@ -0,0 +1,21 @@
+# Working with Lists and Files
+# writelines.py
+
+# This program uses the writelines method to save
+# a list of strings to a file.
+
+def main():
+  # Create a list of strings.
+  cities = ['New York', 'Boston', 'Atlanta', 'Dallas']
+
+  # Open a file for writing.
+  outfile = open('cities.txt','w')
+
+  # Write the list to the file.
+  outfile.writelines(cities)
+
+  # Close the file
+  outfile.close()
+
+# Call the main function.
+main()
