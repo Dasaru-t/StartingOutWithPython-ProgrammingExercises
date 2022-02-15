97b87d5598471b7f71a4afd9165d8bcdb6a81133
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-02-15 19:31:27 +0530

    Created random_numbers_print.py

---
+++ Chapter_7/random_numbers_print.py
@@ -0,0 +1,27 @@
+# random_numbers_print.py
+# This program assigns random numbers to
+# a two-dimensional list.
+import random
+
+# Constants for rows and columns
+ROWS = 3
+COLS = 4
+
+def main():
+  # Create a two-dimensional list.
+  values = [[0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],] 
+            
+  # Fill the list with random numbers.
+  for r in range(ROWS):
+    for c in range(COLS):
+      values[r][c] = random.randint(1,100)
+
+  # Display the random numbers.
+  for r in range(ROWS):
+    for c in range(COLS):
+      print(values[r][c])
+
+# Call the main function.
+main()
