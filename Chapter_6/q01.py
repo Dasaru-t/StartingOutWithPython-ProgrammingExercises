4756e36eaf8159cd138f2b228cf066a6c88a4bd5
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-03-02 22:47:22 +0530

    Updated q01.py

--- Chapter_6/q01.py
+++ Chapter_6/q01.py
@@ -4,18 +4,18 @@
 # all of the values in the numbers.txt file.
 
 def main():
-  # Open the write.txt file for reading.
-  numbers_file = open('numbers.txt', 'r')
+  # Open the numbers.txt file for reading
+  numbers_file = open("numbers.txt","r")
 
   # Read all the lines from the file.
   for line in numbers_file:
-    # Convert line to a int.
+    # Convert line to a float.
     number = int(line)
-    # display the number.
+    # Display the number
     print(number)
 
   # Close the file.
   numbers_file.close()
 
- # Call the main function.
+# Call the main function
 main()
