dbe196d2362d61330a1a9896d4fd342c1dc78707
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-03-24 16:16:46 +0530

    Updated q06.py

--- Chapter_7/q06.py
+++ Chapter_7/q06.py
@@ -1,1 +1,50 @@
 # 6. Larger Than n
+
+import random
+
+# List_size is used as a constant for the
+# size of the list.
+LIST_SIZE = 6
+
+def main():
+  # Get user inputs
+  number = user_input()
+
+  # Create a list with random numbers
+  my_list = create_list()
+
+  # Get numbers larger than n 
+  greater_than_n(my_list,number)
+
+
+def user_input():
+
+  n = int(input("Enter a number (1-100): "))
+  
+  return n
+
+
+def create_list():
+
+  # Create a list to hold random numbers
+  my_list = []
+
+  for count in range(LIST_SIZE):
+    my_list.append(random.randint(1,100))
+
+  print("Numbers in list -",my_list)
+
+  return my_list
+
+
+def greater_than_n(my_list,number):
+  # Create a new list to hold values greater than n
+  new_list=[]
+  for values in my_list:
+    if values > number:
+      new_list.append(values)
+
+  print("Numbers greater than",number,"-",new_list) 
+    
+# Call the main function
+main()
