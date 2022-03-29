37d37554c7984b0aa68de4fbe79b33906b08f9f7
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-03-29 17:58:22 +0530

    Created tictactoegame.py

--- tictactoegame.py
+++ tictactoegame.py
@@ -4,8 +4,13 @@ game = [[0,0,0],
         [0,0,0],
         [0,0,0],]
 
-game[0][1] = 1
-print("   A  B  C")
+def game_board(game_map, player=0, row=0, column=0, just_display = False):
+  print("   A  B  C")
+  if not just_display:
+    game_map[row][column] = player 
+  for count,row in enumerate(game_map):
+    print(count,row)
+  return game_map
 
-for count,row in enumerate(game):
-  print(count,row)
+game = game_board(game, just_display=True)
+game = game_board(game,player = 1, row=2, column=1)
