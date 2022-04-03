104663904573b1d943c531c4ff2efb9e7d40bffc
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-03 18:37:16 +0530

    Updated Chapter_7/q09.py

--- Chapter_7/q09.py
+++ Chapter_7/q09.py
@@ -1,1 +1,23 @@
 # 9. Population Data
+
+def main():
+  population_list = into_a_list()
+  print(population_list)
+
+def into_a_list():
+  # Open USPopulation file for reading.
+  populaion_file = open("USPopulation.txt","r")
+
+  # Read the lines from USPopulation file
+  population_list = populaion_file.readlines()
+
+  # Convert each element to an int.
+  index = 0
+  while index < len(population_list):
+    population_list[index] = int(population_list[index])
+    index += 1
+
+  return population_list
+
+
+main()
