7d79bfc6454aa965567a1515afc135e9ae47e326
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-07 15:45:46 +0530

    Crated Chapter_7/q11.py

--- Chapter_7/q11.py
+++ Chapter_7/q11.py
@@ -2,33 +2,54 @@
 
 def main():
 
-  # constants for rows and columns
+  # Constants for rows and columns
   ROWS = 3
   COLS = 3
 
-  # create a two dimensional list.
+  # Create a two dimensional list.
   values = [[0,0,0],
             [0,0,0],
             [0,0,0]]
 
-  print("Enter numbers from 1 to 9")
+    # need 9 numbers from user. numbers should be between 1-9 and 
+    # each number should be identical. 
+
+  # Get user inputs
+  print("Enter numbers from 1 to 9.each number should be identical.")
   for r in range(ROWS):
     for c in range(COLS):
       print("row",r + 1,"column",c + 1,end = "")
       values[r][c] = int(input(": "))
+  print()
+
 
   # Get the totals from functions and assign them to variables
   r1,r2,r3 = sum_of_rows(values)
   c1,c2,c3 = sum_of_cols(values)
   d1,d2 = sum_of_diagonal(values)
+
+  # Display the random numbers
+  print("Your square is below")
+  r_list = [r1,r2,r3]
+  n = 0
+  # Display row sum with square
+  for r in values:
+    print(r,"=",r_list[n])
+    n+=1
+  # Display column sum with square
+  print("",c1,  c2,  c3)
+  # Display diagonal sum
+  print("\n1st diagonal =",d1)
+  print("2nd diagonal =",d2)
+  print() 
+
+  # determines whether the list is a Lo Shu Magic Square.
+  if r1==r2==r3==c1==c2==c3==d1==d2:
+    print("It is a Lo Shu Magic Square.")
+  else:
+    print("It is not a Lo Shu Magic Square.")
       
   
-  #display the random numbers
-  for r in range(ROWS):
-    for c in range(COLS):
-      print(values[r][c])     
-
-
 # Get sum of rows
 def sum_of_rows(values):
   r1 = 0
