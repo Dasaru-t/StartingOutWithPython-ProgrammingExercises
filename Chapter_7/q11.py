125e4cae6b98b4310dee99881a0bef5dffc8f768
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-07 12:39:04 +0530

    Crated Chapter_7/q11.py

--- Chapter_7/q11.py
+++ Chapter_7/q11.py
@@ -11,16 +11,72 @@ def main():
             [0,0,0],
             [0,0,0]]
 
-  print("Enter value for")
+  print("Enter numbers from 1 to 9")
   for r in range(ROWS):
     for c in range(COLS):
       print("row",r + 1,"column",c + 1,end = "")
       values[r][c] = int(input(": "))
+
+  # Get the totals from functions and assign them to variables
+  r1,r2,r3 = sum_of_rows(values)
+  c1,c2,c3 = sum_of_cols(values)
+  d1,d2 = sum_of_diagonal(values)
+      
   
   #display the random numbers
   for r in range(ROWS):
     for c in range(COLS):
       print(values[r][c])     
+
+
+# Get sum of rows
+def sum_of_rows(values):
+  r1 = 0
+  for num in values[0]:
+    r1 += num
+
+  r2 = 0
+  for num in values[1]:
+    r2 += num
+    
+  r3 = 0
+  for num in values[2]:
+    r3 += num
+
+  # Return values
+  return r1,r2,r3
+
+
+# Get sum of columns
+def sum_of_cols(values):
+  c1 = 0
+  for num in range(3):
+    c1 += values[num][0]
+
+  c2 = 0
+  for num in range(3):
+    c2 += values[num][1]
+
+  c3 = 0
+  for num in range(3):
+    c3 += values[num][2]
+
+  # Return values
+  return c1,c2,c3
+
+
+# Get sum of diagonal
+def sum_of_diagonal(values):
+  d1 = 0
+  for num in range(3):
+    d1 += values[num][num]
+    
+  d2 = 0
+  for num in range(3):
+    d2 += values[num][2-num]
+
+  # Return values
+  return d1, d2
 
 
 main()
