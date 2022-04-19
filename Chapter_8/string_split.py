9c5f73317d8cb8c892d55a01993fd89fe24e7280
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-20 00:03:11 +0530

    Updated string_split.py

--- Chapter_8/string_split.py
+++ Chapter_8/string_split.py
@@ -1,2 +1,15 @@
 # string_split.py
 # This program demonstrates the split method.
+
+def main():
+  # Create a string with multiple words.
+  my_string = "one two three four"
+
+  # Split the string.
+  word_list = my_string.split()
+
+  # Print the list of words.
+  print(word_list)
+  
+# Call the main function.
+main()
