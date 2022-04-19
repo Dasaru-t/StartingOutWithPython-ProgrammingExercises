cd68cebc3b7d19ebbafb206f7597b7f711b9f200
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-19 23:54:28 +0530

    Updated repetition_operator.py

--- Chapter_8/repetition_operator.py
+++ Chapter_8/repetition_operator.py
@@ -1,3 +1,15 @@
 # repetition_operator.py
 
 # This program demonstrates the repetition operator.
+
+def main():
+  # Print nine rows increasing in length.
+  for i in range(0,10):
+    print('z'*i)
+
+  # Print nine rows decreasing in length.
+  for n in range(8,0,-1):
+    print('z'*n)
+
+# Call the main function.
+main()
