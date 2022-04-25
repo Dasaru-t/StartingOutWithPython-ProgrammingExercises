a5a973e072021be444b29761c0839a13a89e35e0
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-25 22:07:20 +0530

    Updated q01.py

--- Chapter_8/q01.py
+++ Chapter_8/q01.py
@@ -1,1 +1,16 @@
 # 1. Initials
+
+def main():
+  
+  # gets a string containing a person’s first, middle, and last names
+  name = input("Enter person's name: ")
+  
+  # Split the string
+  name_split = name.split()
+  
+  # Display their first, middle, and last initials.
+  for i in name_split:
+    print(i[0].upper(),end=".")
+    
+# Call the main function 
+main()
