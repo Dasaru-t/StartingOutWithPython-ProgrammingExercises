4bdcac3164956de14ce0a65ece470fbe6e63d657
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-26 10:53:29 +0530

    Updated q01.py

--- Chapter_8/q01.py
+++ Chapter_8/q01.py
@@ -8,6 +8,9 @@ def main():
   # Split the string
   name_split = name.split()
   
+  # Display the result.
+  print("Your initials")
+  
   # Display their first, middle, and last initials.
   for i in name_split:
     print(i[0].upper(),end=".")
