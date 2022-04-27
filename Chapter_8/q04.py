441626fcb558758c0ddc14efed252792427d379b
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-27 21:31:02 +0530

    Created q04.py

---
+++ Chapter_8/q04.py
@@ -0,0 +1,27 @@
+# 4. Morse Code Converter
+
+def main():
+  
+  # Get user input
+  string = input("Enter a string: ").upper()
+  
+  # Create two lists for conversion. each index will match in two lists.
+  # Create a list with character
+  character=[" ", ",", ".", "?", "0", "1", "2", "3", "4", "5", "6", "7",\
+               "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", \
+               "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", \
+               "W", "X", "Y", "Z"]
+
+  # Create a list with code
+  morse=[" ", "--..--", ".-.-.-", "..--..", "-----", ".----", "..---", "...--", \
+           "....-", ".....", "-....", "--...", "---..", "----.", ".-", "-...", "-.-.", \
+           "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", \
+           ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.-", "--.."]
+  
+  # Go through each character in user input
+  for i in string:
+    # Find the morse code for each character
+    print(morse[character.index(i)],end=" ")
+    
+# Call the main function   
+main()
