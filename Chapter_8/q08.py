5d173483e621cddbbb9c2df21a4898c2a45e67a0
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-30 21:27:45 +0530

    Created q08.py

---
+++ Chapter_8/q08.py
@@ -0,0 +1,16 @@
+# 8. Sentence Capitalizer
+
+def main():
+    
+  # Get user input as a sentence
+  string = input("Enter a sentence: ")
+
+  # Split the sentence by ". "
+  string_split = string.split(". ")
+
+  for i in string_split:
+    # first character of each sentence capitalized
+    print(i[0].upper(),i[1:],sep="",end=". ")
+
+# Call the main function    
+main()
