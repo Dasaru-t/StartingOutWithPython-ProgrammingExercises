e0f93c2e69834db83793df97549f573f54660220
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-04-30 11:11:49 +0530

    Updated q07.py

--- Chapter_8/q07.py
+++ Chapter_8/q07.py
@@ -6,25 +6,30 @@ def main():
   infile = open("text.txt","r")
   
   # Read the line.
-  lines = infile.readlines()
+  lines = infile.read().splitlines()
   
   upper = 0
   lower = 0
   digits = 0
   space = 0
   
-  for i in lines:
-    print
-    
-    '''''
-    for n in len(i):
-      if i.isupper():
+  for line in lines:
+    for ch in line:
+      if ch.isupper():
         upper += 1
-    
-    
+      elif ch.islower():
+        lower += 1
+      elif ch.isdigit():
+        digits += 1
+      elif ch.isspace():
+        space += 1
+        
   print(upper)
+  print(lower)
+  print(digits)
+  print(space)
+    
   
-  '''''
     
 main()
     
