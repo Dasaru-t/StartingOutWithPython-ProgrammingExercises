c83ec74760c22380339d45fecd0e1c1a0fae2c80
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-03 01:44:28 +0530

    Updated q10.py

--- Chapter_8/q10.py
+++ Chapter_8/q10.py
@@ -3,13 +3,30 @@
 def main():
   
   string = "Hello my name is dasaruu".lower()
+
   string_list = []
-  print(len(string))
-  
-  for i in range (len(string)):
-    string_list.append(string[i])
-    
+  occurence = []
+
+  for i in string:
+    if i.isalnum():
+      if i not in string_list:
+        string_list.append(i)
+        occurence.append(1)
+
+      elif i in string_list:
+        occurence[string_list.index(i)]+=1
+
+  max_occ = max(occurence)
+
+
+  print(string_list[occurence.index(max_occ)])
+  print(max_occ)
+
+
+
   print(string_list)
+  print(occurence)
+    
 
 # Call the main function    
 main()
