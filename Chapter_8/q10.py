0ca04c474661972a5ade7be243e1db5da5eb2ed9
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-02 22:43:22 +0530

    Created q10.py

---
+++ Chapter_8/q10.py
@@ -0,0 +1,15 @@
+# 10. Most Frequent Character
+
+def main():
+  
+  string = "Hello my name is dasaruu".lower()
+  string_list = []
+  print(len(string))
+  
+  for i in range (len(string)):
+    string_list.append(string[i])
+    
+  print(string_list)
+
+# Call the main function    
+main()
