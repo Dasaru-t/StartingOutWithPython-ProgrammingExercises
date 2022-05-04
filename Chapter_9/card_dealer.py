aa42bd84a6fd0861ccd482b968f34321ad2d230e
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-04 22:47:56 +0530

    Created card_dealer.py

---
+++ Chapter_9/card_dealer.py
@@ -0,0 +1,15 @@
+# card_dealer.py
+# In the Spotlight: Using a Dictionary to Simulate a Deck of Cards
+
+# This program uses a dictionary as a deck of cards.
+def main():
+  # Create a deck of cards.
+  #deck = create_deck()
+
+  # Get the number of cards to deal.
+  num_cards = int(input('How many cards should I deal?'))
+
+  # Deal the cards.
+  #deal_cards(deck, num_cards)
+
+main()
