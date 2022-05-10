751f95a8aa74d426ec88e81769821616aa1b2f82
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-10 22:08:12 +0530

    updated q03.py

--- Chapter_9/q03.py
+++ Chapter_9/q03.py
@@ -6,8 +6,26 @@ def main():
              'S':'}','T':']','U':':','V':';','W':'"','X':'<','Y':'>','Z':'0','a':'1',
              'b':'2','c':'3','d':'4','e':'5','f':'6','g':'7','h':'8','i':'9','j':'a',
              'k':'b','l':'c','m':'d','n':'e','o':'f','p':'g','q':'h','r':'i','s':'j',
-             't':'k','u':'l','v':'m','w':'n','x':'o','y':'p','z':'q'}
+             't':'k','u':'l','v':'m','w':'n','x':'o','y':'p','z':'q',' ':' ','.':'~'}
 
+  # Open the d text.txt file for reading
+  infile = open("d text.txt","r")
 
+  # Read the lines.
+  lines = infile.readline()
+  
+  # Close the file.
+  infile.close()
+  
+  # Open the out_text.txt file for writing
+  outfile = open("out_text.txt","w")
 
+  for i in lines:
+    outfile.write(str(encrypt[i]) + "\n")
+    
+  # Close the file.
+  outfile.close()
+  print("Data written to out_text.txt")
+
+# Call the main function.
 main()
