a047f96ab600b38b09f869a3143ad859e638ba99
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-13 14:09:58 +0530

    Created sets.py

--- Chapter_9/sets.py
+++ Chapter_9/sets.py
@@ -26,3 +26,21 @@ print()
 print('The following students play either baseball or basketball:')
 for name in baseball.union(basketball):
   print(name)
+
+# Demonstrate difference of baseball and basketball
+print()
+print('The following students play baseball, but not basketball:')
+for name in baseball.difference(basketball):
+  print(name)
+
+# Demonstrate difference of basketball and baseball
+print()
+print('The following students play basketball, but not baseball:')
+for name in basketball - baseball:
+  print(name)
+
+# Demonstrate symmetric difference
+print()
+print('The following students play one sport, but not both:')
+for name in baseball.symmetric_difference(basketball):
+  print(name)
