87a21163973d2ec1adda683e6282fb5ab5960ddc
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-14 22:59:26 +0530

    Updated pickle_objects.py

--- Chapter_9/pickle_objects.py
+++ Chapter_9/pickle_objects.py
@@ -1,29 +1,40 @@
 # This program demonstrates object pickling.
 import pickle
+
+# main function
 def main():
   
+  # Create an empty dictionary.
   personal = {}
 
+  again = "y" # To control loop repetition
   
-  again = "y"
+  # Get data until the user wants to stop.
   while again.lower() == "y":
     
+    # Get data for a person
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
     weight = int(input("Enter your weight: "))
     
+    # Store it in the dictionary.
     personal[name] = [age,weight]
     
     print()
-    again = input("Do you want to enter another('y for yes'): ")
+    # Does the user want to enter more data?
+    again = input("Enter more data? (y/n): ")
     
     
+  # Open a file for binary writing.
   output_file = open('personal.dat','wb')
+  
+  # Pickle the dictionary.
   pickle.dump(personal,output_file)
+  
+  # Close the file.
   output_file.close()
   print()
   print("write personal data to the personal.dat file.")
   print(personal)
   
-    
 main()
