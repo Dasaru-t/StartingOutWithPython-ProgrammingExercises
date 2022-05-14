c8629f998ab83a0b1688c171ce5b6634db3ade3d
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-14 22:51:57 +0530

    Updated pickle_objects.py

--- Chapter_9/pickle_objects.py
+++ Chapter_9/pickle_objects.py
@@ -1,6 +1,5 @@
 # This program demonstrates object pickling.
 import pickle
-
 def main():
   
   personal = {}
@@ -11,13 +10,20 @@ def main():
     
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
-    weight = int(input("Enter your weight"))
+    weight = int(input("Enter your weight: "))
     
     personal[name] = [age,weight]
     
+    print()
     again = input("Do you want to enter another('y for yes'): ")
     
     
+  output_file = open('personal.dat','wb')
+  pickle.dump(personal,output_file)
+  output_file.close()
+  print()
+  print("write personal data to the personal.dat file.")
   print(personal)
+  
     
 main()
