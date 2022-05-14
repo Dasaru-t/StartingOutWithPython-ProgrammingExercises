b47d76a2ec6a827d21360a452b4bd99561bb2b23
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-14 22:44:21 +0530

    Updated pickle_objects.py

--- Chapter_9/pickle_objects.py
+++ Chapter_9/pickle_objects.py
@@ -1,2 +1,23 @@
 # This program demonstrates object pickling.
 import pickle
+
+def main():
+  
+  personal = {}
+
+  
+  again = "y"
+  while again.lower() == "y":
+    
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    weight = int(input("Enter your weight"))
+    
+    personal[name] = [age,weight]
+    
+    again = input("Do you want to enter another('y for yes'): ")
+    
+    
+  print(personal)
+    
+main()
