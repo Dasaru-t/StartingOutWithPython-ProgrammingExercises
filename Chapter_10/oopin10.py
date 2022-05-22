13135246b351ffc787487d65b4feb89620999682
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-22 22:25:22 +0530

    Updated oopin10.py

--- Chapter_10/oopin10.py
+++ Chapter_10/oopin10.py
@@ -2,6 +2,7 @@ from caroopin10 import Car
 
 car_1 = Car("Chevy","Corvette",2021,"blue")
 car_2 = Car("BMW","M5",2020,"Red")
+car_3 = Car("Toyota","Supra",2008,"Black")
 
 print(car_1.make)
 print(car_1.model)
@@ -18,3 +19,11 @@ print(car_2.color)
 
 car_2.drive()
 car_2.stop()
+
+print(car_3.make)
+print(car_3.model)
+print(car_3.year)
+print(car_3.color)
+
+car_3.drive()
+car_3.stop()
