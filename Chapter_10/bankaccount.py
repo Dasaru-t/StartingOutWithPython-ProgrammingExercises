c6ce28e67838228bb71f28551e8dc3421c02313f
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-05-31 14:23:13 +0530

    Updated bankaccount.py

--- Chapter_10/bankaccount.py
+++ Chapter_10/bankaccount.py
@@ -1,1 +1,32 @@
+# bankaccount.py
 # The BankAccount class simulates a bank account.
+
+class BankAccount:
+
+  # The _ _init_ _ method accepts an argument for
+  # the account's balance. It is assigned to
+  # the _ _balance attribute.
+
+  def __init__(self,bal):
+    self.__balance = bal
+
+  # The deposit method makes a deposit into the
+  # account.
+
+  def deposit(self,amount):
+    self.__balance += amount
+
+  # The withdraw method withdraws an amount
+  # from the account.
+
+  def withdraw(self,amount):
+    if self.__balance >= amount:
+      self.__balance -= amount
+    else:
+      print('Error: Insufficient funds')
+
+  # The get_balance method returns the
+  # account balance.
+
+  def get_balance(self):
+    return self.__balance
