e44bbcbd0676c7b2d7ac009026fea93c68f22a4d
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-06-05 23:01:10 +0530

    Updated bankaccount2.py

--- Chapter_10/bankaccount2.py
+++ Chapter_10/bankaccount2.py
@@ -1,2 +1,6 @@
 # bankaccount2.py
 # The BankAccount class simulates a bank account.
+
+# The _ _init_ _ method accepts an argument for
+# the account's balance. It is assigned to
+# the _ _balance attribute.
