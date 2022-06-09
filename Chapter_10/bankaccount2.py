f78eb9b35d4785f23d83371fc787feeccd09a2a6
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-06-09 23:21:20 +0530

    Updated bankaccount2.py

--- Chapter_10/bankaccount2.py
+++ Chapter_10/bankaccount2.py
@@ -1,12 +1,17 @@
 # bankaccount2.py
 # The BankAccount class simulates a bank account.
 
-# The _ _init_ _ method accepts an argument for
-# the account's balance. It is assigned to
-# the _ _balance attribute.
+class BankAccount:
 
-# The deposit method makes a deposit into the
-# account.
+    # The _ _init_ _ method accepts an argument for
+    # the account's balance. It is assigned to
+    # the _ _balance attribute.
+
+    def __init__(self, bal):
+        self.__balance = bal
+
+    # The deposit method makes a deposit into the
+    # account.
 
 # The withdraw method withdraws an amount
 # from the account.
