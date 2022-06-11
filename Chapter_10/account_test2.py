e223738da9ce3e832674dc5d753d0060c2e52233
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-06-11 23:21:24 +0530

    Updated account_test2.py

--- Chapter_10/account_test2.py
+++ Chapter_10/account_test2.py
@@ -11,3 +11,22 @@ def main():
 
     # Create a BankAccount object.
     savings = bankaccount2.BankAccount(start_bal)
+
+    # Deposit the user's paycheck.
+    pay = float(input('How much were you paid this week? '))
+    print('I will deposit that into your account.')
+    savings.deposit(pay)
+
+    # Display the balance.
+    print(savings)
+
+    # Get the amount to withdraw.
+    cash = float(input('How much would you like to withdraw? '))
+    print('I will withdraw that from your account.')
+    savings.withdraw(cash)
+
+    # Display the balance.
+    print(savings)
+
+# Call the main function.
+main()
