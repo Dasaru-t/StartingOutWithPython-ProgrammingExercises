e5d1fce5dbf938a573b6bd408c84f23f49657da2
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-06-11 23:15:34 +0530

    Created account_test2.py

--- Chapter_10/account_test2.py
+++ Chapter_10/account_test2.py
@@ -2,3 +2,12 @@
 
 # This program demonstrates the BankAccount class
 # with the _ _str_ _ method added to it.
+
+import bankaccount2
+
+def main():
+    # Get the starting balance.
+    start_bal = float(input('Enter your starting balance: '))
+
+    # Create a BankAccount object.
+    savings = bankaccount2.BankAccount(start_bal)
