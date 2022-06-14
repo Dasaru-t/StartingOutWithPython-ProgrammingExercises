25b303219c4cd353733a665e878d8cff11e56d59
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-06-14 23:22:30 +0530

    Updated coin_demo5.py

--- Chapter_10/coin_demo5.py
+++ Chapter_10/coin_demo5.py
@@ -2,3 +2,16 @@
 # creates three instances of the Coin class.
 
 import coin
+
+def main():
+    # Create three objects from the Coin class.
+    coin1 = coin.Coin()
+    coin2 = coin.Coin()
+    coin3 = coin.Coin()
+
+    # Display the side of each coin that is facing up.
+    print('I have three coins with these sides up:')
+    print(coin1.get_sideup())
+    print(coin2.get_sideup())
+    print(coin3.get_sideup())
+    print()
