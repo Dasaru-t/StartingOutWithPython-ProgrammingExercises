703c196a50b881c65ca057520256c2c0131d0080
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-06-17 22:59:59 +0530

    Chapter_10/cellphone.py

--- Chapter_10/cellphone.py
+++ Chapter_10/cellphone.py
@@ -1,2 +1,10 @@
 # cellphone.py
 # The CellPhone class holds data about a cell phone.
+
+class CellPhone:
+
+    # The _ _init_ _ method initializes the attributes.
+    def _ _init_ _(self, manufact, model, price):
+        self._ _manufact = manufact
+        self._ _model = model
+        self._ _retail_price = price
