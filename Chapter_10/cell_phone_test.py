43844a3d0ecc00f8f11f2b661f1296cbbac6e5b0
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2022-07-14 22:05:26 +0530

    Updated cell_phone_test.py

--- Chapter_10/cell_phone_test.py
+++ Chapter_10/cell_phone_test.py
@@ -13,3 +13,5 @@ def main():
     # Display the data that was entered.
     print('Here is the data that you entered:')    
     print('Manufacturer:', phone.get_manufact())
+    print('Model Number:', phone.get_model())
+    print('Retail Price: $', format(phone.get_retail_price(), ',.2f'), sep='')
